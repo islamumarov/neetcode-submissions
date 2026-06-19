c650b540ee7338093933d38fd6edf9ef8ac582ef
Islam Umarov <Ihtyyarovich@gmail.com>
2026-06-19 21:33:50 +0300

    Add: find-median-in-a-data-stream - submission-0

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-0.py
@@ -0,0 +1,17 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.low = []
+        self.high = []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.low, -num)
+        heapq.heappush(self.high, -heapq.heappop(self.low))
+        if len(self.low) < len(self.high):
+            heapq.heappush(self.low, -heapq.heappop(self.high))
+
+    def findMedian(self) -> float:
+        if len(self.low) == len(self.high):
+            return (-self.low[0] + self.high[0])/2
+        return -self.low[0]
+        
