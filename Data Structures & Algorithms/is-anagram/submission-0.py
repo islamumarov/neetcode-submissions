0fd29f51f9fd694bc60762cd8cf8b14d8c9fc69a
Islam Umarov <Ihtyyarovich@gmail.com>
2026-06-19 23:34:55 +0300

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        first = [0] * 26
+        second = [0] * 26
+        for c in s:
+            first[ord(c) - ord('a')] +=1
+        for t1 in t:
+            second[ord(t1) - ord('a')] +=1
+        for i in range(26):
+            if first[i] != second[i]:
+                return False
+        
+        return True
